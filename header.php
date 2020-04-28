2d67a2c49977b48dc227676e5b7b156eef90f20a
Jainam12 <jainamdoshi1210@gmail.com>
2020-04-29 02:00:11 +0530

    index done

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" href="css/header.css"/>
 <div id="header">
-  <h1>Shah And Anchor Kutchhi Engineering College</h1>
-  <h3>Attendance System</h3>
+  <h1 id="header-title">Shah And Anchor Kutchhi Engineering College</h1>
+  <h3 id="header-tagline">Attendance System</h3>
 </div>
--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php include('header.php'); ?>
+
+<!DOCTYPE html>
+  <head>
+    <title>Homepage</title>
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+  </head>
+  <body>
+    <div id="main-container">
+      <div class="card" id="card-student">
+        <a href="student-login.php" class="card-anchor" id="card-anchor-student">
+          <h2>Student Portal</h2>
+          <h4>View attendance details</h4>
+        </a>
+      </div>
+      <div class="card" id="card-teacher">
+        <a href="teacher-login.php" class="card-anchor" id="card-anchor-teacher">
+          <h2>Faculty Portal</h2>
+          <h4>Manage attendance</h4>
+        </a>
+      </div>
+  </body>
+</html>
