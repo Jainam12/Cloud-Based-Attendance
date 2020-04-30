5ad9b885e8605fd0ddc175a8769f10a64ad1db5b
Jainam12 <jainamdoshi1210@gmail.com>
2020-04-30 18:10:11 +0530

    Another

--- header.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="css/header.css"/>
-<div id="header">
-  <h1 id="header-title">Shah And Anchor Kutchhi Engineering College</h1>
-  <h3 id="header-tagline">Attendance System</h3>
-</div>
